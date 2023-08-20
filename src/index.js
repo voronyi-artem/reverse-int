e7af0fae81d96573b7576528cbc576c28108b77a
Artem Voronyi <voronyi.artem@gmail.com>
2023-08-20 17:53:42 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
+
+    let stringNumber = Math.abs(n).toString();
+    let reverseString = '';
+
+    for (let item of stringNumber) {
+        reverseString = item + reverseString
+    }
+    return (+reverseString);
   
 }
